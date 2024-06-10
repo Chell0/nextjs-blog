2d4dc695331c80d9668c921a7fb74bfcbf91ba84
chell0 <machelgabriel@gmail.com>
2024-06-10 23:54:53 +0300

    Update refresh rate

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { client, urlFor } from "./lib/sanity";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
+export const revalidate = 30; // revalidate at most every 30 seconds
+
 async function getData() {
   const query = `
   *[_type == 'blog'] | order(_createdAt desc) {
@@ -27,11 +29,21 @@ export default async function Home() {
       <div className={`grid grid-col-1 md:grid-cols-2 mt-5 gap-5`}>
         {data.map((post, index) => (
           <Card key={index}>
-            <Image src={urlFor(post.titleImage).url()} alt="image" width={350} height={300} className="rounded-t-lg h-[200px] object-cover" />
+            <Image
+              src={urlFor(post.titleImage).url()}
+              alt="image"
+              width={350}
+              height={300}
+              className="rounded-t-lg h-[200px] object-cover"
+            />
 
             <CardContent className={`mt-5`}>
               <h3 className={`text-lg line-clamp-2 font-bold`}>{post.title}</h3>
-              <p className={`text-sm text-gray-600 dark:text-gray-300 mt-2 line-clamp-3`}>{post.smallDescription}</p>
+              <p
+                className={`text-sm text-gray-600 dark:text-gray-300 mt-2 line-clamp-3`}
+              >
+                {post.smallDescription}
+              </p>
               <Button asChild className={`w-full mt-7`}>
                 <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
               </Button>
